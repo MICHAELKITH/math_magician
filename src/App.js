fc5a35344981430083525125d2118459d47d3848
Dev Mike <97337681+MICHAELKITH@users.noreply.github.com>
2023-03-20 23:24:41 +0300

    Add new files and check linters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Math Magician</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit
+        <code>src/App.js</code>
+        and save to reload.
+      </p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
