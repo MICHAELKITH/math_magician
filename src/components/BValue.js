fdd9d799aa383a7e470816a3751936e6b84943e8
Dev Mike <97337681+MICHAELKITH@users.noreply.github.com>
2023-03-28 23:12:45 +0300

    deleted files

--- src/components/BValue.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-const BValue = ({ result }) => <p id="bottom-value">{result}</p>;
-
-BValue.propTypes = { result: PropTypes.string.isRequired };
-
-export default BValue;
--- src/components/apiKey.js
+++
@@ -1,5 +0,0 @@
-// suscribe to api-ninjas and add your key to this file
-
-const apiKey = 'https://api.api-ninjas.com/v1/quotes?category=humor';
-
-export default apiKey;
